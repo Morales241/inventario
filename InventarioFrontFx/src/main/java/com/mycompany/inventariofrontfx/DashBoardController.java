ba3434b6611c86c4fd2a224a392bba85f2c57224
Jesus Morales <jesus.morales245335@potros.itson.edu.mx>
2026-02-09 14:44:28 -0700

    decimo sexto avance del front

--- InventarioFrontFx/src/main/java/com/mycompany/inventariofrontfx/DashBoardController.java
+++ InventarioFrontFx/src/main/java/com/mycompany/inventariofrontfx/DashBoardController.java
@@ -6,7 +6,11 @@ package com.mycompany.inventariofrontfx;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 
 /**
  * FXML Controller class
@@ -15,12 +19,49 @@ import javafx.fxml.Initializable;
  */
 public class DashBoardController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private ToggleGroup menu;
+
+    @FXML
+    private ScrollPane centerContainer;
+    
+    @FXML private ToggleButton btnDashBoard;
+    
+    @FXML private ToggleButton btnInventario;
+    
+    @FXML private ToggleButton btnColaboradores;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        // Escuchamos los cambios en el ToggleGroup
+        menu.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                ToggleButton selectedBtn = (ToggleButton) newValue;
+                String menuText = selectedBtn.getText();
+
+                manejarNavegacion(menuText);
+            } else {
+                // Esto evita que el botón se deseleccione si haces clic de nuevo
+                oldValue.setSelected(true);
+            }
+        });
+    }
+
+    private void manejarNavegacion(String opcion) {
+        System.out.println("Navegando a: " + opcion);
+
+        switch (opcion) {
+            case "DashBoard":
+                // logic para cargar vista dashboard
+                break;
+            case "Inventario":
+                // logic para cargar vista inventario
+                break;
+            case "Usuarios":
+                // logic para submenú usuarios
+                break;
+            default:
+                break;
+        }
+    }
 }
